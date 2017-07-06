930bdfbf24d8a01c59d1fb560a3da745f5883177
Tkachenko AI <alexfisher13@yandex.ru>
2017-07-06 12:35:00 +0300

    Comment commit

--- src/main/java/ru/smartcoder/spring_boot_simple_example/MvcConfig.java
+++ src/main/java/ru/smartcoder/spring_boot_simple_example/MvcConfig.java
@@ -8,10 +8,13 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
 public class MvcConfig extends WebMvcConfigurerAdapter {
     
     @Override
-    public void addViewControllers(ViewControllerRegistry registry) {
+    public void addViewControllers(ViewControllerRegistry registry) { // ViewControllerRegistry - удобная функциональность из библиотеки Spring,
+        // которая позволяет нам не создавать отдельные классы контроллеров для того, чтобы наши Thymeleaf странички показались пользователю.
         registry.addViewController("/base").setViewName("base");
         registry.addViewController("/").setViewName("base");
         registry.addViewController("/hello").setViewName("hello");
     }
 
 }
+
+
